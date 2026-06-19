dd6d9c58a5cc3fb5e0c537536384eee3827f5fd5
Moussa Fofana <mfofana@aguimaweb.com>
2026-06-19 05:46:35 +0000

    Template Pages : 99%

--- wp-content/themes/pfoenergies/inc/style.php
+++ wp-content/themes/pfoenergies/inc/style.php
@@ -28,3 +28,11 @@ function pfoenergies_button(string $url, string $label, string $variant = 'prima
         ]
     );
 }
+
+add_filter('wpcf7_form_elements', function(string $content) {
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+    $content = str_replace('<br />', '', $content);
+    $content = str_replace('<p>', '', $content);
+    $content = str_replace('</p>', '', $content);
+    return $content;
+});
